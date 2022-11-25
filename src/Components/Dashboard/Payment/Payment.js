e1c8fe914316a7e2bda7ee513dbd86a65cf1a5f3
ProgrammerBayzid <mdbayzid2622002@gmail.com>
2022-11-25 13:42:51 +0600

    chekout form

--- src/Components/Dashboard/Payment/Payment.js
+++ src/Components/Dashboard/Payment/Payment.js
@@ -1,5 +1,13 @@
 import React from 'react'
+import { loadStripe } from '@stripe/stripe-js';
+
 import { useLoaderData, useNavigation } from 'react-router-dom';
+
+
+
+
+const stripePromise = loadStripe('pk_test_51M6U64G7PkQ5daFNJ5jIwvzjgdLP0ANXmUoTnVYgQHAGCqCwm2cIsy62dVJ8QbCmVLmThdXNGRVYedVJNfK7p1JN009bkUa0eS');
+
 
 const Payment = () => {
     const data = useLoaderData();
